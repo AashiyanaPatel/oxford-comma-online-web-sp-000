7dd751fb0a1ecd5aca46c90624287d304da3e06c
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-05 03:49:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
-  case array.length
-  when 1
-    "#{array[0]}"
-  when 2
-    array[0..1].join("and")
-  else
-    array[0...-1].join(",")<<", and #{array[-1]}"
-  end
-end
+  if array.length == 1
+      arr.join
+ elsif arr.length == 2
+      arr.join("and")
+  else 
+    arr.each.collect.do |value|
+      if value == arr[-1]
+        arr[-1] = "and" << value
