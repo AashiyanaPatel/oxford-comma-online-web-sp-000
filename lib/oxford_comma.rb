1044ef175df6c76557275e712cadabffa1ec4215
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-05 03:55:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,10 @@
 def oxford_comma(array)
-  if array.length == 1
-      arr.join
- elsif arr.length == 2
-      arr.join("and")
-  else 
-    arr.each.collect.do |value|
-      if value == arr[-1]
-        arr[-1] = "and" << value
-      end
-    end
-    arr.join(",")
+  return array.first if array.size == 1
+  
+  if array.size == 2
+    array.insert(1, "and").join(",")
+  elsif array.size > 2
+    array[-1].insert(0, "and")
+    array.join(",")
   end
 end
